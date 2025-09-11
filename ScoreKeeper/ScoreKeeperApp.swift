ee103f276e931da3113ee9e7136b1f91608c8989
Antonio Anerão <anerao.junior@gmail.com>
2025-09-11 04:29:34 -0500

    Initial Commit

---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Antonio Anerão on 11/09/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
